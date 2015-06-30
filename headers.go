e8924773c41de6438745877a643b2eb6359d4a39
Glenn Goodrich <glenn.goodrich@gmail.com>
2015-06-30 15:38:25 -0400

    Fix tests

--- headers.go
+++ headers.go
@@ -1,8 +1,8 @@
 package cors
 
 const (
-	AccessControlAllowOrigin   = "Access-Control-Allow-Origin"
-	AccessControlAllowMethods  = "Access-Control-Allow-Methods"
-	AccessControlRequestMethod = "Access-Control-Request-Method"
-	Origin                     = "Origin"
+	AccessControlAllowOrigin   string = "Access-Control-Allow-Origin"
+	AccessControlAllowMethods         = "Access-Control-Allow-Methods"
+	AccessControlRequestMethod        = "Access-Control-Request-Method"
+	Origin                            = "Origin"
 )
